f96461f7a6538ce50c01bcee54767ce9808aae21
unknown <kjootaa@gmail.com>
2020-06-06 17:45:18 -0300

    inserindo na tela o tempo e spinner

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+const secondsContainer = document.querySelector('#seconds')
+const minutesContainer = document.querySelector('#minutes')
+const hoursContainer = document.querySelector('#hours')
+const daysContainer = document.querySelector('#days')
+const nextYearContainer = document.querySelector('#year')
+const spinnerLoading = document.querySelector('#loading')
+
 const nextYear = new Date().getFullYear() + 1
 const newYearTime = new Date(`janeiro 01 ${nextYear} 00:00:00`)
+
+nextYearContainer.textContent = nextYear
 
 const updateCountdown = () => {
     const currentTime = new Date()
     const diferenceTime = newYearTime - currentTime  
-    const days = Math.floor(diferenceTime /1000 / 60 / 60 / 24)
+    const days = Math.floor(diferenceTime / 1000 / 60 / 60 / 24)
     const hours = Math.floor(diferenceTime / 1000/ 60 / 60) % 24
     const minutes = Math.floor(diferenceTime / 1000/ 60 ) % 60
     const seconds = Math.floor(diferenceTime / 1000 ) % 60
 
-    console.log(minutes)
+    secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
+    minutesContainer.textContent = minutes < 10 ? '0' + minutes : minutes
+    hoursContainer.textContent = hours < 10 ? '0' + hours : hours
+    daysContainer.textContent = days < 10 ? '0' + days : days
 }
-updateCountdown()
+
+setTimeout(() => {
+    spinnerLoading.remove()
+}, 1000)
+setInterval(updateCountdown, 1000)
