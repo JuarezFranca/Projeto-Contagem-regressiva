386dd8895509efea8c941da839361afa2e85a113
unknown <kjootaa@gmail.com>
2020-06-06 17:15:52 -0300

    calculando o tempo

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const newYearTime = new Date(`janeiro 01 ${nextYear} 00:00:00`)
 const updateCountdown = () => {
     const currentTime = new Date()
     const diferenceTime = newYearTime - currentTime  
-    console.log(diferenceTime)
+    const days = Math.floor(diferenceTime /1000 / 60 / 60 / 24)
+    const hours = Math.floor(diferenceTime / 1000/ 60 / 60) % 24
+    const minutes = Math.floor(diferenceTime / 1000/ 60 ) % 60
+    const seconds = Math.floor(diferenceTime / 1000 ) % 60
+
+    console.log(minutes)
 }
 updateCountdown()
